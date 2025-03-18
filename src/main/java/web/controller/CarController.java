794a29d0f4ccc3758a520fe53abb6b8fd1c65af9
TechEvgeny <v.c_kokoc@mail.ru>
2025-03-18 21:50:17 +0300

    Test1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,20 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.servise.CarService;
+
+@Controller
+public class CarController {
+    @Autowired
+    CarService carService;
+
+    @GetMapping("/cars")
+    public String testCarsMethod(@RequestParam (value = "count", required = false) Integer count, Model model) {
+        model.addAttribute("cars", carService.getCountCars(count));
+        return "car";
+    }
+}
